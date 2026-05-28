30ca25ad1bf100471ec58866cc0abf7f50166c4e
shawnongjk-prog <shawn_ongjk@yahoo.com>
2026-05-28 15:37:36 +0800

    Update delivery banner message for clarity
    Rephrase message for urgent orders and contact method.

--- src/components/DeliveryBanner.tsx
+++ src/components/DeliveryBanner.tsx
@@ -25,7 +25,9 @@ const DeliveryBanner = () => {
       <div className="container mx-auto text-center text-sm font-medium">
        Orders have to be made min. 1 week in advance
       <br />
-       For urgent orders less than 1 week, additional charges apply. Kindly contact us on Whatsapp for more details.
+       For urgent orders less than 1 week, additional charges apply.
+        <br />
+        Kindly contact us on Whatsapp for urgent orders.
         <br />
       🚚 Free Islandwide Delivery for orders over $70!
       </div>
